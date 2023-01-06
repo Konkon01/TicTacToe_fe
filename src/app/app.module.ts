fa3b13b88a446a914ace9891a81cdec5e60f5bf3
Huang Yu <98464106+Konkon01@users.noreply.github.com>
2023-01-06 10:47:18 +0100

    Basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { JoinGameComponent } from './join-game/join-game.component';
+import { GameRoomComponent } from './game-room/game-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JoinGameComponent,
+    GameRoomComponent
   ],
   imports: [
     BrowserModule,
